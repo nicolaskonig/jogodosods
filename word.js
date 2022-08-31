40c5edb72062e24601d6788696405b4f083f3cb0
nicolaskonig <95778639+nicolaskonig@users.noreply.github.com>
2022-08-31 10:53:51 -0300

    Create word.js

---
+++ word.js
@@ -0,0 +1,186 @@
+const wordList = [
+    /*{
+        word: "",
+        hint: ""
+    },*/
+    {
+        word: "desigualdade",
+        hint: "Contrário a todos terem livre acesso a uma vida digna"
+    },
+    {
+        word: "saneamento",
+        hint: "Garantia de saúde da população e do meio ambiente"
+    },
+    {
+        word: "sustentabilidade",
+        hint: "Tem o foco na preservação"
+    },
+    {
+        word: "paz",
+        hint: "Ausência de conflitos"
+    },
+    {
+        word: "preservar",
+        hint: "Utilização racional de recursos"
+    },
+    {
+        word: "transparente",
+        hint: "As instituições precisam dar conhecimento de suas ações ao povo em geral"
+    },
+    {
+        word: "moral",
+        hint: "Base do relacionamento político, social e pessoal"
+    },
+    {
+        word: "cultura",
+        hint: "Dá sentido de pertencimento às pessoas"
+    },
+    {
+        word: "futuro",
+        hint: "Há de vir"
+    },
+    {
+        word: "emprego",
+        hint: "Utilização prática"
+    },
+    {
+        word: "agricultura",
+        hint: "Prática no solo, que consequentemente dá comida no prato"
+    },
+    {
+        word: "movimento",
+        hint: "Conexão de várias pessoas na mesma direção"
+    },
+    {
+        word: "racionalizar",
+        hint: "Estratégia de utilização dos bens básicos"
+    },
+    {
+        word: "redistribuir",
+        hint: "Forma de tornar a sociedade mais igualitária"
+    },
+    {
+        word: "oportunidade",
+        hint: "Garantir o trabalho de qualidade para toda a população"
+    },
+    {
+        word: "respeito",
+        hint: "Não distinguir sexo, raça ou religião de qualquer e todo indivíduo"
+    },
+    {
+        word: "proteger",
+        hint: "Garantir a sobrevivência da fauna e da flora"
+    },
+    {
+        word: "progresso",
+        hint: "Evolução da humanidade"
+    },
+    {
+        word: "alimento",
+        hint: "Elimina a fome no mundo"
+    },
+    {
+        word: "solidariedade",
+        hint: "União dos países em favor do bem estar da humanidade"
+    },
+    {
+        word: "educar",
+        hint: "Toda escola segue esse princípio básico dos ODS"
+    },
+    {
+        word: "energia",
+        hint: "Essencial para todos os setores, desde a indústria ao meio rural"
+    },
+    {
+        word: "natureza",
+        hint: "Sem preservá-la, o mundo corre o iminente risco de acabar"
+    },
+    {
+        word: "inovar",
+        hint: "Essencial para criar economias alternativas que beneficiem a todos"
+    },
+    {
+        word: "cuidado",
+        hint: "Nem só a terra precisa, mas os rios e mares também"
+    },
+    {
+        word: "consumismo",
+        hint: "É um dos motivos pelos quais os recursos naturais da terra se esgotam"
+    },
+    {
+        word: "burocracia",
+        hint: "Um dos empecilhos que tornam as instituições ineficazes"
+    },
+    {
+        word: "acessibilidade",
+        hint: "É a chave que conecta a maioria dos princípios dos ODS"
+    },
+    {
+        word: "empoderamento",
+        hint: "Forma de garantir tratamento equalitário entre os gêneros"
+    },
+    {
+        word: "prosperidade",
+        hint: "O crescimento econômico sustentável é a única forma de alcançar esse status"
+    },
+    {
+        word: "vulnerabilidade",
+        hint: "Lado fraco de um assunto ou questão"
+    },
+    {
+        word: "fome",
+        hint: "É uma sensação fisiológica"
+    },
+    {
+        word: "nutrir",
+        hint: "Alimentar"
+    },
+    {
+        word: "resilir",
+        hint: "Tornar nulo"
+    },
+    {
+        word: "ecossistema",
+        hint: "Comunidade de organismos de várias espécies e seu ambiente"
+    },
+    {
+        word: "vacina",
+        hint: "Suspensão de microrganismos patogênicos, mortos ou atenuados, introduzida num organismo"
+    },
+    {
+        word: "imunizar",
+        hint: "Proteger-se no âmbito da saúde"
+    },
+    {
+        word: "poluir",
+        hint: "Tornar sujo"
+    },
+    {
+        word: "restaurar",
+        hint: "Pôr em bom estado"
+    },
+    {
+        word: "cooperar",
+        hint: "Atuar, juntamente com outros, para um mesmo fim"
+    },
+    {
+        word: "reciclagem",
+        hint: "Processo de reaproveitamento"
+    },
+    {
+        word: "empreender",
+        hint: "Usar o tempo e as suas melhores competências técnicas e comportamentais com autonomia para criar valor"
+    },
+    {
+        word: "privacidade",
+        hint: "Não se deve invadir a de ninguém"
+    },
+    {
+        word: "desastre",
+        hint: "Evento, acontecimento que causa sofrimento e grande prejuízo"
+    },
+    {
+        word: "risco",
+        hint: "Não se deve invadir a de ninguém"
+    },
+]
